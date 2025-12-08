d16ef36dac49185d9a6857cb10f7005a1e7d51dc
Ivan Zaburdaev <ivanlom227@gmail.com>
2025-12-08 17:31:45 +0300

    new iteration

--- convex/votes.ts
+++ convex/votes.ts
@@ -58,3 +58,31 @@ export const getVotesByUserId = query({
     return await ctx.db.query("votes").withIndex("byUserId", q => q.eq("userId", userId)).collect();
   },
 });
+
+export const getVotesByGroup=query({
+  handler: async (ctx)=>{
+    const results= await ctx.db.query('votes').collect()
+    const resultVotes=new Map()
+    results.map(result=>{
+      const existResult=resultVotes.get(result.choice)
+      if(existResult){
+        resultVotes.set(result.choice,existResult+1)
+      }
+      else{
+        resultVotes.set(result.choice,1)
+      }
+    })
+    const result = Array.from(resultVotes, ([key, votes]) => {
+      const [candidateId, ...nameParts] = key.split(' ');
+      const name = nameParts.join(' ').trim();
+      return {
+        candidateId: candidateId.replace(/[^0-9]/g, ''), // берем только цифры для id
+        name,
+        votes,
+      };
+    });
+    
+    return result
+    
+  }
+})
